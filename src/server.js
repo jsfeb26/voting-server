1cf3329f037de4864815251067c9877cacb2b6b6
jsfeb26 <jsfeb26@gmail.com>
2015-11-01 21:02:54 -0500

    Subscribe to redux store changes and send current state over socket.io

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
   const io = new Server().attach(8090);
+
+  // subscribe a listener to the store that reads the current state,
+  // turns it into a plain JavaScript object, and emits it as a 'state' event
+  // on the Socket.io server
+  store.subscribe(
+    // JSON-serialized snapshot of the state is ent over all active Socket.io connections
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+  });
 }
